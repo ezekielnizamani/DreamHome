30d933c17e73e3204ec8b616ac620b2ed41b17ec
hizqeel <ezekielnizamani0@gmail.com>
2024-01-07 01:12:11 +0500

    wrote csv handler

---
+++ csvHandler.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/csv"
+	"log"
+	"os"
+)
+
+func readCsvFile(filePath string) [][]string {
+	f, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal("Unable to read input file "+filePath, err)
+	}
+	defer f.Close()
+
+	csvReader := csv.NewReader(f)
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatal("Unable to parse file as CSV for "+filePath, err)
+	}
+	return records
+}
+func getHeader(data *[][]string) []string {
+
+	header := []string((*data)[0])
+	(*data) = (*data)[1:]
+	return header
+}
