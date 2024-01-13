865c495d20a21864ebcc599d9a5156ed2085391f
hizqeel <ezekielnizamani0@gmail.com>
2024-01-13 19:28:31 +0500

    made ge request and read body

--- crwalerFunctions.go
+++ crwalerFunctions.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type Reader interface {
+	Read(p []byte) (n int, err error)
+}
 
 func crwalForsythCounty(address string) {
-	fmt.Println(address)
+	var url string = "https://lrcpwa.ncptscloud.com/forsyth/searchProperty.aspx"
+	getInitials(url)
+}
+func getInitials(url string) {
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("Erorr %v", err)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	content := string(body)
+	println(content)
+
 }
